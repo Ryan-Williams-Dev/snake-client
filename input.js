5e56b5ad2781cd7b8cfaab87f1c80d85d9fcc7a6
Ryan Williams <r.d.williams.ca@gmail.com>
2022-02-17 01:07:42 +0000

    added consts

--- input.js
+++ input.js
@@ -1,7 +1,10 @@
+const { connect } = require("./client");
+let connection;
 
 
-const setupInput = function () {
+const setupInput = function (conn) {
   const stdin = process.stdin;
+  connection = conn;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
@@ -11,6 +14,26 @@ const setupInput = function () {
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
+  }
+  switch(key) {
+  case 'w':
+    connection.write('Move: up')
+    break;
+  case 'a':
+    connection.write('Move: left')
+    break;
+  case 'd':
+    connection.write('Move: right')
+    break;
+  case 's':
+    connection.write('Move: down')
+    break;
+  case ' ':
+    connection.write('Say: Hide Yo Snakes!');
+    break;
+  case 'e':
+    connection.write('Say: just end me now.');
+    break;
   }
 };
 
